2389fd054b184820a8262fd99655c0edf2fe3e6a
cesar11b <katanavivas@gmail.com>
2022-03-31 11:09:35 -0600

    Cambios

---
+++ Domain/Entities/Empleado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Empleado
+    {
+
+        public int Id { get; set; }
+        public String Cedula { get; set; }
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public String Direccion { get; set; }
+        public String Telefono { get; set; }
+        public String Email { get; set; }
+
+    }
+}
---
+++ Infraestructure/Repository/BinaryEmpleadoRepository.cs
@@ -0,0 +1,78 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Repository
+{
+    public class BinaryEmpleadoRepository: IEmpleadoModel
+    {
+
+        private RAFContext<Activo> context;
+        private const int SIZE = 568;
+        public void Add(Empleado t)
+        {
+            try
+            {
+                context.Create<Empleado>(t);
+            }
+            catch (IOException)
+            {
+                throw;
+            }
+        }
+
+        public void Delete(Empleado t)
+        {
+            try
+            {
+                context.Delete<Empleado>(t);
+            }
+            catch (IOException)
+            {
+                throw;
+            }
+        }
+
+        public List<Empleado> Find()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Empleado GetById(int id)
+        {
+            try
+            {
+                return context.Get<Empleado>(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<Empleado> Read()
+        {
+            try
+            {
+                return context.GetAll<Empleado>();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public int Update(Empleado t)
+        {
+            return context.Update<Empleado>(t);
+        }
+
+
+
+    }
+}
